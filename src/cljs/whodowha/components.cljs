ca7e5063dc945c35bb770ed26f39a0c63d767366
David Nielsen <david@graveyard.dk>
2015-11-11 21:47:03 +0100

    leger lidt med defcomponentk

--- src/cljs/whodowha/components.cljs
+++ src/cljs/whodowha/components.cljs
@@ -1,1 +1,2 @@
 (ns whodowha.components)
+
--- src/cljs/whodowha/core.cljs
+++ src/cljs/whodowha/core.cljs
@@ -1,7 +1,7 @@
 (ns ^:figwheel-always whodowha.core
   (:require [om.core :as om :include-macros true]
-            [om-tools.core :refer-macros [defcomponent]]
-            [om-tools.dom :as d :include-macros true]))
+            [om-tools.core :refer-macros [defcomponent defcomponentk]]
+            [om-tools.dom :as dom :include-macros true]))
 
 (enable-console-print!)
 
@@ -11,10 +11,48 @@
 
 (defonce app-state (atom {:text "Hello world!s"}))
 
+(defcomponent button [data owner]
+  (render [_]
+    (dom/button #js {:onClick #(js/alert "Cliiiick.. !!")} "Add contact")))
 
-(defcomponent load-indicator [data owner]
+(defcomponent input-text [data owner]
+  (render [_]
+    (dom/input )))
+
+(defcomponentk label [[:data text] state owner]
+  (render [_]
+    (dom/span {:style {:width (-> (/ text (- max min))
+                                         (* 100)
+                                         (int)
+                                         (str "%"))}
+             :on-mouse-enter #(swap! state assoc :show-text? true)
+             :on-mouse-leave #(swap! state assoc :show-text? false)
+             }
+            (dom/label
+              (when (:show-text? @state)
+                (str text "/" text))))))
+
+(defcomponentk progress-bar
+  [[:data value {min 0} {max 100}] state]
+  (render [_]
+    (dom/div nil
+             (dom/span
+               {:style          {:width (-> (/ value (- max min))
+                                            (* 100)
+                                            (int)
+                                            (str "%"))}
+                :on-mouse-enter #(swap! state assoc :show-value? true)
+                :on-mouse-leave #(swap! state assoc :show-value? false)}
+             (when (:show-value? @state)
+               (str value "/100"))))))
+
+(defcomponent  load-indicator [data owner]
               (render [_]
-                      (d/div "Hellow from component")))
+                (dom/div
+                  (om/build label {:text 300 :noget-state "state"})
+                  (om/build progress-bar {:value 66})
+                  (om/build button nil)
+                  (om/build input-text nil))))
 
 (om/root load-indicator app-state
          {:target (. js/document (getElementById "whodowha"))})
